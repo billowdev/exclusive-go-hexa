328ea2c07a6dcc1ccf67d6ae1811c463605b1bd8
Akkarapon Phikulsri <akkarapon@looksocial.com>
2024-08-27 17:16:08 +0700

    wip: temporal

--- internal/adapters/temporal/worker/worker.go
+++ internal/adapters/temporal/worker/worker.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"log/slog"
 
-	repositories "github.com/billowdev/exclusive-go-hexa/internal/adapters/repositories/activity"
 	"github.com/billowdev/exclusive-go-hexa/internal/adapters/temporal/activities"
 	"github.com/billowdev/exclusive-go-hexa/internal/adapters/temporal/workflows"
 	"github.com/billowdev/exclusive-go-hexa/pkg/configs"
@@ -34,18 +33,48 @@ func WorkflowClient() client.Client {
 
 	return c
 }
+
 func RegisterTemporalWorkflow(c client.Client, db *gorm.DB) {
-	w := worker.New(c, "email_worker", worker.Options{})
-
-	w.RegisterWorkflow(workflows.LoggingWorkflow)
-
-	repo := repositories.NewActivityRepository(db)
-	loggingActivity := &activities.LoggingActivity{Repo: repo}
-
-	w.RegisterActivity(loggingActivity)
-
-	if err := w.Run(worker.InterruptCh()); err != nil {
-		log.Fatalln("Unable to start email_worker", err)
+	workerOptions := worker.Options{
+		// MaxConcurrentActivityExecutionSize: 1, // Limits the number of concurrent activities
+		// MaxConcurrentWorkflowTaskPollers:   1, // Ensures only one workflow task poller
 	}
-	// return nil
+	ticketWorker := worker.New(c, "TICKET_TASK_QUEUE", workerOptions)
+	ticketWorker.RegisterWorkflow(workflows.TicketPurchaseWorkflow)
+	ticketWorker.RegisterActivity(activities.ValidateRequestActivity)
+	ticketWorker.RegisterActivity(activities.ProcessPaymentActivity)
+	ticketWorker.RegisterActivity(activities.ReserveTicketActivity)
+	ticketWorker.RegisterActivity(activities.SendConfirmationActivity)
+
+	registerWorker := worker.New(c, "REGISTER_TASK_QUEUE", workerOptions)
+	registerWorker.RegisterWorkflow(workflows.RegistrationWorkflow)
+	registerWorker.RegisterActivity(activities.RegisterDataActivity)
+	registerWorker.RegisterActivity(activities.CheckDataActivity)
+	registerWorker.RegisterActivity(activities.VerifyIdentitiesActivity)
+	registerWorker.RegisterActivity(activities.UpdateStatusActivity)
+
+	// go func() {
+	// 	if err := ticketWorker.Run(worker.InterruptCh()); err != nil {
+	// 		log.Fatalf("Failed to start ticketWorker: %v", err)
+	// 	}
+	// }()
+	// go func() {
+	// 	if err := registerWorker.Run(worker.InterruptCh()); err != nil {
+	// 		log.Fatalf("Failed to start registerWorker: %v", err)
+	// 	}
+	// }()
+	// select {}
+	err := ticketWorker.Start()
+	if err != nil {
+		log.Fatalf("Failed to start worker: %v", err)
+	}
+	err = registerWorker.Start()
+	if err != nil {
+		log.Fatalf("Failed to start worker: %v", err)
+	}
+	select {}
+	// if err := registerWorker.Run(worker.InterruptCh()); err != nil {
+	// 	log.Fatalf("Failed to start registerWorker: %v", err)
+	// }
+
 }
